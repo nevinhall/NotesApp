281583446971ed46b768465fd88ffda742626684
nevinhall <46722804+nevinhall@users.noreply.github.com>
2020-06-04 08:12:17 +0100

    create commands using yargs

--- app.js
+++ app.js
@@ -1,7 +1,50 @@
-const validator = require('validator')
+const yargs = require('yargs')
+const chalk = require('chalk')
 const getNotes = require('./notes.js')
 
-const msg = getNotes()
+//Customize yargs
+yargs.version('1.1.0')
 
-console.log(msg)
-console.log(validator.isEmail('nevinexample@gmail.com'))
+//add, remove, read, list
+
+//create add command
+yargs.command({
+    command: 'add',
+    describe: 'add a new note',
+    handler: function () {
+        console.log('adding a new note...')
+
+    }
+})
+
+//create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'remove a new note',
+    handler: function () {
+        console.log('removing a new note...')
+
+    }
+})
+
+//create read command
+yargs.command({
+    command: 'read',
+    describe: 'read the notes',
+    handler: function () {
+        console.log('read a note')
+
+    }
+})
+
+//create list command
+yargs.command({
+    command: 'list',
+    describe: 'list the notes',
+    handler: function () {
+        console.log('listing all the notes')
+
+    }
+})
+console.log(process.argv)
+console.log(yargs.argv)
